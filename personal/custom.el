0721f74673d20b44b2dea7655087584de6db00b8
Wang Dongsheng <wds@wds1.cn>
2018-03-01 17:57:42 +0800

    personal: add custom.el
    Signed-off-by: Wang Dongsheng <wds@wds1.cn>

---
+++ personal/custom.el
@@ -0,0 +1,14 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "SteelBlue" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 122 :width normal :foundry "unknown" :family "Hermit"))))
+ )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(pkgbuild-mode zop-to-char zenburn-theme which-key volatile-highlights vlf unfill undo-tree solarized-theme smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters popup-imenu paxedit ov operate-on-number multiple-cursors move-text mode-line-bell magit imenu-list imenu-anywhere helm-swoop helm-projectile helm-ls-git helm-gtags helm-git guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine git gist flycheck expand-region elisp-slime-nav editorconfig easy-kill dynamic-spaces discover-my-major diminish diff-hl crux company color-theme-solarized color-theme-sanityinc-solarized browse-kill-ring beacon anzu ace-window)))
