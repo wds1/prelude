549e48a05fe1081824af584687c7b9d25c4186b9
Wang Dongsheng <wds@wds1.cn>
2018-03-09 10:43:56 +0800

    misc: popup-imenu popup-switcher and background
    Signed-off-by: Wang Dongsheng <wds@wds1.cn>

---
+++ personal/preload/init-popup-switcher.el
@@ -0,0 +1,9 @@
+;;; .emacs -- init-popup-switcher
+;;; Commentary:
+;;; Code:
+
+(global-set-key [f2] 'psw-switch-buffer)
+(global-set-key [f3] 'psw-switch-function)
+(setq psw-popup-position 'center)
+
+;;; init-popup-switcher.el ends here
